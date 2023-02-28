06b1de23ec84a4f41c732c1d01490dc36c801012
Mamath68 <mathieu.stamm@gmail.Com>
2023-02-28 15:09:23 +0100

    Avancé

--- index.php
+++ index.php
@@ -2,9 +2,13 @@
 
 <?php
 include "Hilton.php";
-include "Regent.php";
 include "Reservation.php";
 
+$hilton = new Hilton("Hotel", "Hilton", "10 route de la Gare, 67000 Strasbourg", 30, 3, 27);
+// $regent = new Regent ()
+$r1 = new Reservation("Virgile", "Gribello", 17, 01 - 01 - 2021, 01 - 01 - 2021);
+$r2 = new Reservation("Mika", "Murmann", 3, 11 - 03 - 2021, 11 - 03 - 2021);
+$r3 = new Reservation("Mika", "Murmann", 4, 01 - 04 - 2021, 01 - 04 - 2021);
 
 
 
