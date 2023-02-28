728ec6261a00e1acbd56980bee9678e0e3e38478
Mamath68 <mathieu.stamm@gmail.Com>
2023-02-28 15:06:45 +0100

    Avancé

--- Hilton.php
+++ Hilton.php
@@ -2,16 +2,63 @@
 
 class Hilton
 {
-
+    private $nameHotel;
     private $nom;
     private $adresse;
-    private $nbChambre;
+    private $nbChambres;
+    private $nbChambreRes;
+    private $nbChambreDispo;
 
-    public function __construct($nom, $adresse, $nbChambre)
+
+    public function __construct($nameHotel, $nom, $adresse, $nbChambres, $nbChambreRes, $nbChambreDispo)
     {
+        $this->nameHotel = $nameHotel;
         $this->nom = $nom;
         $this->adresse = $adresse;
-        $this->nbChambre = $nbChambre;
+        $this->nbChambres = $nbChambres;
+        $this->nbChambreRes = $nbChambreRes;
+        $this->nbChambreDispo = $nbChambreDispo;
+
     }
 
+    public function getNom()
+    {
+        return $this->nom;
+    }
+    public function getAdresse()
+    {
+        return $this->adresse;
+    }
+    public function getNbChambres()
+    {
+        return $this->nbChambres;
+    }
+    public function getNbChambreRes()
+    {
+        return $this->nbChambreRes;
+    }
+    public function getNbChambreDispo()
+    {
+        return $this->nbChambreDispo;
+    }
+    public function setNom($nom)
+    {
+        $this->nom = $nom;
+    }
+    public function setAdresse($adresse)
+    {
+        $this->adresse = $adresse;
+    }
+    public function setNbChambres($nbChambres)
+    {
+        $this->nbChambres = $nbChambres;
+    }
+    public function setNbChambreRes($nbChambreRes)
+    {
+        $this->nbChambreRes = $nbChambreRes;
+    }
+    public function setNbChambreDispo($nbChambreDispo)
+    {
+        $this->nbChambreDispo = $nbChambreDispo;
+    }
 }
--- Reservation.php
+++ Reservation.php
@@ -2,25 +2,62 @@
 
 class Reservation
 {
-    private $nbChambreRes;
-    private $nbChambreDispo;
-
-    public function __construct($nbChambreRes, $nbChambreDispo)
-    {
-        $this->nbChambreRes = $nbChambreRes;
-        $this->nbChambreDispo = $nbChambreDispo;
-    }
-
-}
-class Client extends Reservation
-{
     private $prenom;
     private $nom;
+    private $ChambreRes;
+    private DateTime $dateDebut;
+    private DateTime $dateDeFin;
 
-    public function __construct($prenom, $nom)
+    public function __construct($nom, $prenom,$ChambreRes, $dateDebut, $dateDeFin)
     {
         $this->prenom = $prenom;
         $this->nom = $nom;
+        $this->ChambreRes = $ChambreRes;
+        $this->dateDebut = new DateTime($dateDebut);
+        $this->dateDeFin = new DateTime($dateDeFin);
     }
+
+    public function getPrenom()
+    {
+        return $this->prenom;
+    }
+    public function getNom()
+    {
+        return $this->nom;
+    }
+    public function getChambreRes()
+    {
+        return $this->ChambreRes;
+    }
+    public function getDateDebut()
+    {
+        return $this->dateDebut;
+    }
+    public function getDateDeFin()
+    {
+        return $this->dateDeFin;
+    }
+    public function setPrenom()
+    {
+        return $this->prenom;
+    }
+    public function setNom($nom)
+    {
+        $this->nom = $nom;
+    }
+    public function setNbChambreRes($ChambreRes)
+    {
+        $this->ChambreRes = $ChambreRes;
+    }
+    public function setDateDebut($dateDebut)
+    {
+        $this->dateDebut = $dateDebut;
+    }
+    public function setDateDeFin($dateDeFin)
+    {
+        $this->dateDeFin = $dateDeFin;
+    }
+
+
 
 }
